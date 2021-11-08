854252d2d153721b6fcd551915cf6f9bcc5fa270
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-11-08 19:45:31 +0100

    Base setup

---
+++ Core/Core/Models/BEReview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Models
+{
+    public class BEReview
+    {
+        private int Reviewer;
+        private int Movie;
+        private int Grade;
+        private DateTime ReviewDate;
+    }
+}
